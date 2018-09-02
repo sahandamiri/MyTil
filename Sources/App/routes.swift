6ba098ffbd434267e7c5b1362b102b3ff006af5a
Sajadamiripouya <42603226+Sajadamiripouya@users.noreply.github.com>
2018-09-02 16:59:00 +0400

    Nothing

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,4 +6,11 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    router.post("api", "acronyma") {req -> Future<Acronym> in
+        return try req.content.decode(Acronym.self)
+            .flatMap(to: Acronym.self) { acronym in
+                return acronym.save(on: req);
+                
+        }
+    }
 }
